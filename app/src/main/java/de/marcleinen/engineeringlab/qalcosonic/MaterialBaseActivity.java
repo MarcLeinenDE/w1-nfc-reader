8be49afe6f8418c5589e38d563544319dfd86844
Marc Leinen <34336531+MarcLeinenDE@users.noreply.github.com>
2026-09-07 10:04:28 +0200

    fix: retain ViewCompat inset request API

--- app/src/main/java/de/marcleinen/engineeringlab/qalcosonic/MaterialBaseActivity.java
+++ app/src/main/java/de/marcleinen/engineeringlab/qalcosonic/MaterialBaseActivity.java
@@ -50,6 +50,6 @@ abstract class MaterialBaseActivity extends AppCompatActivity {
                     startBottom + bars.bottom);
             return windowInsets;
         });
-        ViewCompat.requestApplyWindowInsets(root);
+        ViewCompat.requestApplyInsets(root);
     }
 }
